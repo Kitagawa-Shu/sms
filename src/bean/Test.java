a5238af92216ea43dd71c64279bde112bde3454d
Zun0330 <ymg2465085@stu.o-hara.ac.jp>
2025-06-16 12:09:47 +0900

    6/16 土屋 得点Bean

---
+++ src/bean/Test.java
@@ -0,0 +1,36 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+
+	/**
+	 * クラス名:student
+	 */
+	private Student student;
+
+	/**
+	 * クラス名:classNum
+	 */
+	private String String;
+
+	/**
+	 * クラス名:subject
+	 */
+	private Subject subject;
+
+	/**
+	 * クラス名:school
+	 */
+	private School school;
+
+	/**
+	 * クラス名:no
+	 */
+	private int no;
+
+	/**
+	 * クラス名:point
+	 */
+	private int point;
+}

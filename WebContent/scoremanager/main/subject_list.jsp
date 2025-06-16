7939d59a1dc3c189255c71bd2913ee6b58a57955
yakumo <ymg2465041@stu.o-hara.ac.jp>
2025-06-16 12:26:56 +0900

    6/16 subject_listを完成させるためにSubjectCreateActionをつくっている

---
+++ WebContent/scoremanager/main/subject_list.jsp
@@ -0,0 +1,44 @@
+<%-- 科目管理一覧画面JSP --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="/common/base.jsp" >
+	<c:param name="title">
+		得点管理システム
+	</c:param>
+
+	<c:param name="scripts"></c:param>
+
+	<c:param name="content">
+		<section class="me=4">
+			<h2 class="h3 mb-3 fw-norma bg-secondary bg-opacity-10 py-2 px-4">科目管理</h2>
+			<div class="my-2 text-end px-4">
+				<a href="SubjectCreate.action">新規登録</a>
+			</div>
+
+			<c:choose>
+				<c:when test="${subjects.size()>0 }">
+					<div>検索結果：${students.size() }件</div>
+					<table class="table table-hover">
+						<tr>
+							<th>科目コード</th>
+							<th>科目名</th>
+
+						</tr>
+						<c:forEach var="subjects" items="${subjects }">
+							<tr>
+								<td>${subjects.cd }</td>
+								<td>${subjects.name }</td>
+								<td>${subjects.school }</td>
+								<td class="text-center"></td>
+								<td><a href="SubjectCreatAction?no=${subjects.no }">変更</a></td>
+							</tr>
+						</c:forEach>
+					</table>
+				</c:when>
+
+			</c:choose>
+
+		</section>
+	</c:param>
+</c:import>
---
+++ src/bean/SubjectCreateAction.java
@@ -0,0 +1,55 @@
+package bean;
+
+import java.util.List;
+
+import javax.security.auth.Subject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.ClassNumDao;
+import dao.StudentDao;
+import tool.Action;
+
+public class SubjectCreateAction extends Action {
+
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("user");
+
+        String subject_cd = "";
+        String subject_name = "";
+        int subject = 0;
+
+        Subject subjects = new Subject();
+        StudentDao studentDao = new StudentDao();
+        ClassNumDao classNumDao = new ClassNumDao();
+
+        // パラメータ取得
+        subject_cd = req.getParameter("cd");
+
+
+        subject = SubjectDao.get(subject_cd);
+
+
+        List<String> class_num_set = classNumDao.filter(teacher.getSchool());
+
+
+
+        subject_cd = subject_cd.getCd());
+        subject_name = subject_name.getName();
+
+
+
+        req.setAttribute("cd", subject_cd);
+        req.setAttribute("name", subject_name);
+        req.setAttribute("class_num", class_num);
+        req.setAttribute("is_attend", isAttend);
+
+        req.setAttribute("class_num_set", class_num_set);
+
+        req.getRequestDispatcher("subject_list.jsp").forward(req, res);
+    }
+}

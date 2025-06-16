31f712177831e0f31dce7784f0449241d2404fd0
Zun0330 <ymg2465085@stu.o-hara.ac.jp>
2025-06-16 12:11:37 +0900

    6/17 土屋 得点Bean

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -33,4 +33,54 @@ public class Test implements Serializable {
 	 * クラス名:point
 	 */
 	private int point;
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public String getString() {
+		return String;
+	}
+
+	public void setString(String string) {
+		String = string;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+
 }

b4b40a41b508de23d23d52f791677e19046e373d
Zun0330 <ymg2465085@stu.o-hara.ac.jp>
2025-06-18 09:44:37 +0900

    6/18 クラス別成績一覧Bean

---
+++ src/bean/TestListSubject.java
@@ -0,0 +1,77 @@
+package bean;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class TestListSubject implements Serializable {
+
+	/**
+	 * クラス名:entYear
+	 */
+	private int entYear;
+
+	/**
+	 * クラス名:studentNo
+	 */
+	private String studentNo;
+
+	/**
+	 * クラス名:studentName
+	 */
+	private String studentName;
+
+	/**
+	 * クラス名:classNum
+	 */
+	private String classNum;
+
+	/**
+	 * クラス名:points
+	 */
+	private Map<Integer,Integer> points;
+
+	public int getEntYear() {
+		return entYear;
+	}
+
+	public void setEntYear(int entYear) {
+		this.entYear = entYear;
+	}
+
+	public String getStudentNo() {
+		return studentNo;
+	}
+
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getClassNum() {
+		return classNum;
+	}
+
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+
+	public Map<Integer, Integer> getPoints() {
+		return points;
+	}
+
+	public void setPoints(Map<Integer, Integer> points) {
+		this.points = points;
+	}
+
+
+
+
+
+}

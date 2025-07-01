740f4f4444de1d245391d3d89aff1b753c5989a3
yakumo <ymg2465041@stu.o-hara.ac.jp>
2025-07-01 10:52:00 +0900

    7/1科目変更修正完了

--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -4,8 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.School;
 import bean.Subject;
+import bean.Teacher;
 import dao.SubjectDao;
 import tool.Action;
 
@@ -15,19 +15,24 @@ public class SubjectUpdateAction extends Action {
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
 
-        HttpSession session = req.getSession();
-        String schoolCd = (String) session.getAttribute("school_cd");
+//        HttpSession session = req.getSession();
+//        String schoolCd = (String) session.getAttribute("school_cd");
+//
+//
+//        School school = new School();
+//        school.setCd(schoolCd);
 
+    	/* セッションデータの取得 */
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
 
-        School school = new School();
-        school.setCd(schoolCd);
 
 
         String cd = req.getParameter("cd");
 
 
         SubjectDao dao = new SubjectDao();
-        Subject subject = dao.get(cd, school);
+        Subject subject = dao.get(cd, teacher.getSchool());
 
 
         req.setAttribute("cd", subject.getCd());

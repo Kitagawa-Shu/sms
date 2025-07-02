ec2b58afb9bc45c71d2e6cb1c35172b205feb6f1
Todakoma <ymg2465017@stu.o-hara.ac.jp>
2025-07-02 14:16:47 +0900

    一部修正済み

--- src/scoremanager/main/TestRegistExecuteAction.java
+++ src/scoremanager/main/TestRegistExecuteAction.java
@@ -1,6 +1,8 @@
 package scoremanager.main;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,8 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.Subject;
 import bean.Teacher;
 import bean.Test;
+import dao.ClassNumDao;
 import dao.StudentDao;
 import dao.SubjectDao;
 import dao.TestDao;
@@ -72,13 +76,73 @@ public class TestRegistExecuteAction extends Action {
 			testDao.save(testlist);
 			req.getRequestDispatcher("test_regist_done.jsp").forward(req, res);
 		} else { // エラーメッセージがある場合
+			String entYearStr = "";
+			String classNum = "";
+			String subjectStr = "";
+			String timesNumStr = "";
+			int entYear = 0;
+			int num = 0;
 
-			/* 元の一覧表示のための処理 */
-			req.setAttribute("errors", errors); // ★ エラー文を渡す
-		    req.setAttribute("f1", req.getParameter("ent_year"));   // 入学年度
-		    req.setAttribute("f2", req.getParameter("class_num"));  // クラス
-		    req.setAttribute("f3", req.getParameter("subject"));    // 科目コード
-		    req.setAttribute("f4", req.getParameter("count"));      // テスト回数
+			ClassNumDao classNumDao = new ClassNumDao();
+			LocalDate todaysDate = LocalDate.now();
+			List<Test> tests = null;
+			int year = todaysDate.getYear();
+
+			// リクエストパラメーターの取得
+			entYearStr = req.getParameter("f1");
+			classNum = req.getParameter("f2");
+			subjectStr = req.getParameter("f3");
+			timesNumStr = req.getParameter("f4");
+
+
+
+			// 学校コードからクラス番号の一覧を取得
+			List<String> list = classNumDao.filter(teacher.getSchool());
+
+			// 入学年度セットの作成
+			List<Integer> entYearSet = new ArrayList<>();
+			for (int i = year - 10; i < year + 11; i++) {
+				entYearSet.add(i);
+			}
+
+			// ★ 科目一覧を取得
+			List<Subject> subjectList = subjectDao.filter(teacher.getSchool());
+
+			// ★ テスト回数リスト
+			List<Integer> testNumList = Arrays.asList(1, 2);
+
+
+			if (entYearStr != null) {
+				// 数値に変換
+				entYear = Integer.parseInt(entYearStr);
+			}
+
+			/* timesNumStrをnumに数値変換して代入 */
+			if (timesNumStr != null) {
+				num = Integer.parseInt(timesNumStr);
+			}
+
+
+			if (entYear !=0 && !classNum.equals("0") && subjectList !=null && testNumList !=null ) {
+				/* subjectを1件取得する */
+				subject = subjectDao.get(student_no, teacher.getSchool());
+
+				tests = testDao.filter( entYear, classNum, subject, num,teacher.getSchool());
+			}
+
+
+			req.setAttribute("f1", entYearStr);
+			req.setAttribute("f2", classNum);
+			req.setAttribute("f3", subjectStr);
+			req.setAttribute("f4", timesNumStr);
+
+
+			// JSPに渡すデータをセット
+			req.setAttribute("class_num_set", list);
+			req.setAttribute("ent_year_set", entYearSet);
+			req.setAttribute("subject_set", subjectList); // ★ 科目一覧
+			req.setAttribute("times_num_set", testNumList);     // ★ テスト回数
+			req.setAttribute("tests", tests);
 
 			// 登録画面にフォワード
 			req.getRequestDispatcher("TestRegist.action").forward(req, res);
